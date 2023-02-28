8ebf67e03ebdf3d6db32a5e46f13d9b21d48fef0
marlongr <marloncleitongross@gmail.com>
2023-02-27 23:06:50 -0300

    ajuste inicial

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -73,20 +73,20 @@
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
             <!-- Brand Logo -->
             <a href="{{ url('/') }}" class="brand-link">
-                <img src="{{ asset('img/AdminLTELogo.png') }}" alt="Razza PRO"
-                    class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Razza PRO</span>
+                {{-- <img src="{{ asset('img/LogoRazza.png') }}" alt="Razza PRO"
+                    class="brand-image img" style="opacity: .8"> --}}
+                <span class="brand-text font-weight-light">Sitema Razza PRO</span>
             </a>
 
             <!-- Sidebar -->
             <div class="sidebar">
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-                    <div class="image">
-                        <img src="{{ asset('img/avatar5.png') }}" class="img-circle elevation-2" alt="User Image">
-                    </div>
+                    {{-- <div class="image">
+                        <img src="{{ asset('img/user1-128x128.jpg') }}" class="img-circle elevation-2" alt="User Image">
+                    </div> --}}
                     <div class="info">
-                        <a href="#" class="d-block">Nome usuário logado</a>
+                        <a href="#" class="d-block">User: Marlon Gross</a>
                     </div>
                 </div>
 
