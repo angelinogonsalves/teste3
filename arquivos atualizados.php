de765306aca10e1a853f0c4df2e1f8761c814fcb
Angelino Techsocial <agonsalves@techsocial.com.br>
2023-06-01 22:22:13 -0300

    arquivos atualizados

---
+++ arquivos atualizados.php
@@ -0,0 +1,9 @@
+app/Http/Controllers/AlunoController.php
+app/Http/Controllers/HomeController.php
+app/Http/Controllers/PagSeguroController.php
+app/Http/Controllers/PedidoController.php
+app/Http/Services/PagseguroService.php
+app/Models/Pedido.php
+resources/views/aluno/aluno-home.blade.php
+resources/views/pedido/lista-pedido.blade.php
+
